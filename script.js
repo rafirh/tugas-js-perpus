878f73c4d885516cedaf585cd9847dc8b0c0b5f0
Rafi Rahman <rafirahmann18@gmail.com>
2022-01-26 21:33:37 +0700

    ngantuk

--- script.js
+++ script.js
@@ -4,9 +4,20 @@ document.getElementById('tanggal').valueAsDate = new Date();
 const kolom = ["nis","nama","kelas","buku","jumlah","tanggal"];
 
 //Deklarasi variable
-const buku = ["Matematika","IPA","IPS","PPKN","PJOK","Bahasa Inggris","Bahasa Indonesia","PJOK"];
+const judulbuku = ["Matematika","IPA","IPS","PPKN","PJOK","Bahasa Inggris","Bahasa Indonesia","PJOK"];
 
 //Menampilkan daftar buku
 for (let index = 0; index < buku.length; index++) {
-    document.getElementById('buku').innerHTML += "<option value=" + buku[index] + ">" + buku[index] + "</option>";
+    document.getElementById('buku').innerHTML += "<option value=" + judulbuku[index] + ">" + judulbuku[index] + "</option>";
+}
+
+function kirim(){
+    for (let index = 0; index < kolom.length; index++) {
+        eval('var ' + kolom[index] + ' = []');
+
+        for (let j = 0; j < kolom[index].length + 1; j++) {
+            eval('var ' + kolom[index] + [j] +' = document.getElementById('+ kolom[index] +').value');
+        }
+        
+    }
 }
